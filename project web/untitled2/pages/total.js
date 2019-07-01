0a3435b48840683f6bff9c901ceed14ee6278cff
iftachr <49309794+iftachr@users.noreply.github.com>
2019-07-01 22:40:04 +0300

    a

--- project web/untitled2/pages/total.js
+++ project web/untitled2/pages/total.js
@@ -1,14 +1,20 @@
 function validateFormSignin() {
-  var x = document.forms["mysignin"]["username"].value;
-  if (x.length<8) {
+  let x = document.forms["mysignin"]["username"].value;
+  let x2 =document.forms["mysignin"]["psw"].value;
+  if (x.length<6){
+    alert("user name must be longer than 6");
+    return  false;
+  }
+  if (x2.length<8) {
     alert("Password must be longer than 8 digits");
     return false;
   }
 }
+
 function validateFormSearch() {
-  var x = document.forms["mysignin"]["username"].value;
-  if (x=="") {
-    alert("Password must be longer than 8 digits");
+  let y = document.forms["searchForm"]["searchInput"].value;
+  if (y=="") {
+    alert("search Input must not be empty");
     return false;
   }
 }
